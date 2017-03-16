6bad6bc7727db11406eeb4d5d691b5d7e1b60982
vrathsmill <vlrathsmill@gmail.com>
2017-03-16 16:15:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, - 1)
+}
